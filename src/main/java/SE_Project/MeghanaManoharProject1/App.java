0f0e820e56b3fda4b813b102b577333d0db61f4c
Meghana <meghanamanohar.10@gmail.com>
2023-11-02 13:11:11 +0530

    successfully done all files

---
+++ src/main/java/SE_Project/MeghanaManoharProject1/App.java
@@ -0,0 +1,13 @@
+package SE_Project.MeghanaManoharProject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! I am Meghana Manohar and this is my Maven Project" );
+    }
+}
